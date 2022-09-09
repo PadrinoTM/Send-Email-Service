87755ea8f0c5a21b1d837f0c4e996aa10eaa24b7
PadrinoTM <tmidowu@gmail.com>
2022-09-10 00:18:25 +0100

    Email Service A{PI

---
+++ EmailSenders/Controllers/MailController.cs
@@ -0,0 +1,34 @@
+﻿using EmailSenders.Entity;
+using EmailSenders.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EmailSenders.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private readonly IMailService mailService;
+        public MailController(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
+        [HttpPost("SendEmail")]
+        public async Task<IActionResult> SendMail([FromForm] MailRequest request)
+        {
+            try
+            {
+                await mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+    }
+}
---
+++ EmailSenders/Services/IMailServices.cs
@@ -0,0 +1,10 @@
+﻿using EmailSenders.Entity;
+using System.Threading.Tasks;
+
+namespace EmailSenders.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
